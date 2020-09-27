42e2bd76e83c09d5198bfe4303f7cf89813ffdac
Bangnany <qkrwotj203@gmail.com>
2020-09-27 15:18:05 +0900

    Profile Schema filled

--- model/profile.js
+++ model/profile.js
@@ -4,15 +4,38 @@ const mongoose = require('mongoose')
 
 const profileSchema = mongoose.Schema(
     {
-        user: {},
-        handle: {},
-        company: {},
-        website: {},
-        location: {},
-        status: {},
-        skills: {},
-        bio: {},
-        githubusername: {},
+        user: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'user'
+        },
+        handle: {
+            type: String,
+            required: true,
+            max: 40
+        },
+        company: {
+            type: String
+        },
+        website: {
+            type: String
+        },
+        location: {
+            type: String
+        },
+        status: {
+            type: String,
+            required: true
+        },
+        skills: {
+            type: [String],
+            required: true
+        },
+        bio: {
+            type: String
+        },
+        githubusername: {
+            type: String
+        },
         experience: [],
         education: [],
         social: {}
